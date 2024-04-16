d9d6578389ca68bd611b42252a656bbc0015fa81
Eduardsss <126756209+Eduardsss@users.noreply.github.com>
2024-04-16 14:13:57 +0300

    Update add_fruit.php

--- 1.daļa/add_fruit.php
+++ 1.daļa/add_fruit.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Fruit</title>
+    <title>Pievieno gramatu</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Add Fruit</h1>
+    <h1>Pievieno gramatu</h1>
     <form action="add_fruit_process.php" method="POST">
         <label for="name">Name:</label><br>
         <input type="text" id="name" name="name" required><br><br>
 
-        <label for="calories">Calories:</label><br>
+        <label for="calories">Cik ir pieejamas:</label><br>
         <input type="number" id="calories" name="calories" required><br><br>
 
         <input type="submit" value="Add Fruit">
